0e8297dc1b71f21f15319e659a3d945f62b12f10
Vanika Kapoor <vanikapoor31@gmail.com>
2026-05-18 22:16:06 +0530

    movingzeroes.cpp

---
+++ movingzeroes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
+
+int main() {
+    vector<int> nums = {0,1,0,3,0,5,0,7,9,0,12};
+    Solution s;
+    s.moveZeroes(nums);
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    return 0;
+}
