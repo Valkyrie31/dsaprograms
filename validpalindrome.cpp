899adb62c47fdc4bf040d7889c2a995b56ff414f
Vanika Kapoor <vanikapoor31@gmail.com>
2026-05-20 11:50:25 +0530

    validpalindrome.cpp

---
+++ validpalindrome.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+
+    string s = "A man, a plan, a canal: Panama";
+
+    Solution obj;
+
+    bool result = obj.isPalindrome(s);
+
+    cout << boolalpha << result;
+
+    return 0;
+}
