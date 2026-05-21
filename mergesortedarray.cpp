47f493cbf65f0c320867512bef82040f44cec630
Vanika Kapoor <vanikapoor31@gmail.com>
2026-05-21 11:03:07 +0530

    mergesortedarray.cpp

---
+++ mergesortedarray.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        int i = m - 1;          // Last valid element in nums1
+        int j = n - 1;          // Last element in nums2
+        int k = m + n - 1;      // Last position of nums1
+
+        // Merge from back
+        while (i >= 0 && j >= 0) {
+
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            k--;
+        }
+
+        // Copy remaining nums2 elements
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+};
+
+int main() {
+
+    vector<int> nums1 = {1,2,3,0,0,0};
+    vector<int> nums2 = {2,5,6};
+
+    int m = 3;
+    int n = 3;
+
+    Solution s;
+
+    s.merge(nums1, m, nums2, n);
+
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
